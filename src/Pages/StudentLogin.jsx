86790d14f88cf0354bb66e75b721e239d7240d17
Roy <mutuaroymulinge@gmail.com>
2026-01-26 01:42:43 +0300

    add the logic layer and the login form with error message

--- src/Pages/StudentLogin.jsx
+++ src/Pages/StudentLogin.jsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
+function StudentLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-function StudentLogin(){
-    return(
-        <div>
+  async function handleLogin(e) {
+    e.preventDefault();
+    setError("");
 
-        </div>
-    );
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/student-dashboard");
+    } catch {
+      setError("Invalid credentials");
+    }
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+      <input value={email} onChange={e => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      <button>Login</button>
+      {error && <p>{error}</p>}
+    </form>
+  );
 }
 
 export default StudentLogin;
