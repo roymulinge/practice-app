032c2e5813d3e3f73c8ff0b0e864c1578f02ea12
Roy <mutuaroymulinge@gmail.com>
2026-01-29 21:47:50 +0300

    add style to main screen

--- src/Pages/StudentSignup.jsx
+++ src/Pages/StudentSignup.jsx
@@ -24,8 +24,13 @@ function StudentSignup() {
       [e.target.name]: e.target.value,
     });
   };
+ return(
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-50 py-12 px-4 sm:px-6 lg:px-8">
 
+    </div>
+ );
   
 }
+
 
 export default StudentSignup;
