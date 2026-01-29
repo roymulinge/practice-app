128cbcd9b1b1d99b7b3938de38c26ee8a9459866
Roy <mutuaroymulinge@gmail.com>
2026-01-29 21:41:17 +0300

    add useState import,  email and password  import,  auth and db import,   and navigation import

---
+++ src/Pages/StudentSignup.jsx
@@ -0,0 +1,9 @@
+import {useState} from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../firebase";
+import { useNavigate } from "react-router-dom";
+function StudentSignup() {
+    
+}
+
+export default StudentSignup;
